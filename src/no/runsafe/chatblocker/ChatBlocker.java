175f98d756f97fccac8f888d85c851bf779bfec0
Kruithne <personal@stanleybatch.co.uk>
2012-04-20 23:40:01 +0100

    Skeleton

---
+++ src/no/runsafe/chatblocker/ChatBlocker.java
@@ -0,0 +1,12 @@
+package no.runsafe.chatblocker;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class ChatBlocker extends RunsafePlugin
+{
+    @Override
+    protected void PluginSetup()
+    {
+        // Add components
+    }
+}
