d8740c3ebcf97c0b7df30bb12ee475fd050c4624
Morten Nilsen <morten@runsafe.no>
2012-04-22 19:33:27 +0200

    Change to configuration subsystem

--- src/no/runsafe/chatblocker/ChatBlocker.java
+++ src/no/runsafe/chatblocker/ChatBlocker.java
@@ -1,28 +1,27 @@
 package no.runsafe.chatblocker;
 
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.configuration.IConfigurationDefaults;
 import no.runsafe.framework.configuration.IConfigurationFile;
 
 import java.io.InputStream;
 
-public class ChatBlocker extends RunsafePlugin implements IConfigurationFile, IConfigurationDefaults
+public class ChatBlocker extends RunsafePlugin implements IConfigurationFile
 {
-    @Override
-    protected void PluginSetup()
-    {
-        this.addComponent(PlayerEvents.class);
-    }
+	@Override
+	protected void PluginSetup()
+	{
+		this.addComponent(PlayerEvents.class);
+	}
 
-    @Override
-    public InputStream getDefaultConfiguration()
-    {
-        return getResource(Constants.DEFAULT_CONFIGURATION_FILE);
-    }
+	@Override
+	public InputStream getDefaultConfiguration()
+	{
+		return getResource(Constants.DEFAULT_CONFIGURATION_FILE);
+	}
 
-    @Override
-    public String getConfigurationPath()
-    {
-        return Constants.CONFIGURATION_FILE;
-    }
+	@Override
+	public String getConfigurationPath()
+	{
+		return Constants.CONFIGURATION_FILE;
+	}
 }
